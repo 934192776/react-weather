631f67739845a91d5e6e7b5ab996abb9c8ac2b8e
jinghui <934192776@qq.com>
2025-04-21 20:21:31 +1000

    added axios to  the weather

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Weather from "./Weather";
+
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello from React</h1>
+      <Weather city="Sydney"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
